1547548f1cbae087f808963aff61ef168563c455
SATYANARAYANA <yvvsatyanarayana999@gmail.com>
2026-02-27 22:55:14 +0530

    database connection added

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from Core.Config import settings
+from Core.Database import engine, Base
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": f"{settings.DATABASE_URL} is connected successfully!"}
